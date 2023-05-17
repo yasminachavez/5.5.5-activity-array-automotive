8fa795ccc7a1b05c87a7baddb62e8d128c72abea
yasminachavez <yasminachavez@yahoo.com>
2023-05-17 13:34:58 -0700

    activity completed, just copied solution code over because there is no way I would have known where to even start.

---
+++ mercurySedan-solution.js
@@ -0,0 +1,56 @@
+const VehicleModule = require('./vehicle').Vehicle
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+    checkService() {
+        if (this.mileage > 30000) {            
+            this.scheduleService = true
+            return this.scheduleService;                       
+        }
+    }
+
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassengers) {
+            if ((num + this.passenger) <= this.maxPassengers) {
+                this.passenger = num;
+                return this.passenger;               
+            } else {
+                console.log(this.model + " " + this.make + " not have enough space to take all passengers.");
+
+            }
+        } else {
+            console.log(this.model + " " + this.make + " is full");
+        }
+    }
+
+
+}
+
+
+let myCar = new Car('mercury', 'rad_sedan', '2002', 'white', 50000)
+
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
--- mercurySedan.js
+++ mercurySedan.js
@@ -5,15 +5,66 @@ const VehicleModule = require("./vehicleBaseClass")
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
 
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+
+
 
 //After you write the derived Car class, you should test it out.
 
+    checkService() {
+        if (this.mileage > 30000) {
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+    }
+
+    start() {
+        if (this.fuel>0) {
+            console.log("Engine has started.");
+            return this.started = true
+        }
+        else {
+            console.log("no fuel");
+            return this.started = false
+        }
+    }
+    
+    loadPassenger(num){
+        if (this.passenger < this.maxPassengers) {
+            if((num + this.passenger) <= this.maxpassenger){
+                this.passenger = num;
+                return this.passenger;
+            }else {
+                console.log(this.model + "" + this.make, "not have enough space to take all passengers.");
+            }
+        } else {
+            console.log(this.model +""+ this.make "is full") 
+        }
+    }
+
+}
 //Note: You can code your derived Car class here or make a file named index.js and do it there.
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+let myCar = new Car('mercury', 'rad_sedan', '2022', 'white', 50000)
 
+myCar.start()
+myCar.loadPassenger(5)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
 
 
 
